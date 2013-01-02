9561bc7f67c0c96715de342a7f4ec8e4a300e4d0
Alex Weber <alexwebr@gmail.com>
2013-01-02 01:21:56 -0600

    Changed echo to work with FreeBSDs /bin/sh

--- maximal_test.sh
+++ maximal_test.sh
@@ -2,9 +2,9 @@
 
 assert_eq() {
   if [ $2 -eq $3 ]; then
-    echo "$1 [32mpass[0m\c"
+    echo -n "$1 [32mpass[0m"
   else
-    echo "$1 [31mfail[0m\c"
+    echo -n "$1 [31mfail[0m"
   fi
 }
 
@@ -15,13 +15,13 @@ for i in 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19; do
   # 3. Values repeat repeatedly at the end of the predicted frequency (distribution)
   period=$(echo "2 ^ $i - 1" | bc)
   sample_size=$(echo "$period * 3" | bc)
-  echo "TEST: $i-bit maximal LFSR with $sample_size samples... \c"
+  echo -n "TEST: $i-bit maximal LFSR with $sample_size samples... "
   "./${i}bit" | head -n "$sample_size">temporary_file
   <temporary_file sort>temporary_file_sorted
   assert_eq coverage $(<temporary_file_sorted uniq | wc -l | tr -d " ") $period
-  echo ", \c"
+  echo -n ', '
   assert_eq bias $(<temporary_file_sorted uniq -c | sort -nu | wc -l | tr -d " ") 1
-  echo ", \c"
+  echo -n ', '
   assert_eq distribution $(head -n $((period+1)) <temporary_file | tail -n 1) $(head -n 1 <temporary_file)
   echo
 done
